bdc3fc63ed2daa3d91326a637f230aac26fda7ae
Andreas Hellwig <a5308y@gmail.com>
2020-03-22 16:47:29 +0100

    Remove unnecessary linked file

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, '/home/decidim'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/master.key', 'config/application.yml'
+append :linked_files, 'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'public/system'
